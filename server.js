d82fe6266e4c851607a6a0fbf8aba76634eee4e8
namrata70 <dkritikar4sv@gmail.com>
2016-10-16 17:20:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,23 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articleOne  = 
+{
+    title:"article-one",
+    heading:"article-one! kritika rai dwivedi",
+    date:"oct 16,2016"
+    content:
+        <p>
+                this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.
+        </p>
+        <p>
+                this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.
+        </p>
+        <p>
+                this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.
+        </p>"
+    
+};
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
