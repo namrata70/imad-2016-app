3b5199ce9078c8b66abbd8459b9010f655066fb0
namrata70 <dkritikar4sv@gmail.com>
2016-10-16 18:48:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element = document.getElementById ('main-text');
 element.innerHTML= "KRITIKA RAI DWIVEDI";
+var element = document.getElementById('krd');
+krd.onclick = function()
+{
+  krd.style.marginLeft = '100px'; 
+};
