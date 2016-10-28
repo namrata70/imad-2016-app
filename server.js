82ee6857d73180cbc833021fc2e7a8571f961709
namrata70 <dkritikar4sv@gmail.com>
2016-10-28 21:19:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg');
+var config = {
+    user:'namrata70',
+    database:'namrata70',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:'process.emv. DB_PASSWORD'
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -71,6 +79,22 @@ return htmlTemplate;
 }
  app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+var pool = new Pool (config);
+app.get('/test-db', function(req, res)
+{//make a select request
+// return a response with the result
+  pool.query('SELECT * FROM test', function(err, result)
+  {
+      if(err)
+      {
+          res.status(500).send(err.toString());
+      }
+      else
+      {
+          res.send(JSON.sringify(result));
+      }
+  })  
 });
 
 app.get('/:articleName', function (req, res) {
