6ab094feda7daceef0a96a6288ffc1403910fbbe
namrata70 <dkritikar4sv@gmail.com>
2016-11-12 00:50:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@
     submit.onclick = function()
     {
         //create a request object
-        var request = new  createXMLHttpRequest();
+        var request = new  XMLHttpRequest();
         request.onreadystatechange = function()
         {
             if (request.readyState === XMLHttpRequest.DONE)
