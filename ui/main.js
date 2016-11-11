7d897ee5e59ef5eea7ade24067e8dc05f9add344
namrata70 <dkritikar4sv@gmail.com>
2016-11-12 00:43:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,29 +1,33 @@
-
-    //submit username/password to login
+//submit username/password to login
     var submit = document.getElementById('submit_btn');
-    submit.onclick = function(){
+    submit.onclick = function()
+    {
         //create a request object
         var request = new  createXMLHttpRequest();
-        request.onreadystatechange = function(){
-            if (request.readyState === XMLHttpRequest.DONE){
-                if(request.status === 200){
-                    console.log(' user logged in');
+        request.onreadystatechange = function()
+        {
+            if (request.readyState === XMLHttpRequest.DONE)
+            {
+                if(request.status === 200)
+                {
                 alert('logged in succesfully!');
-                }else if (request.status === 403){
+                }else if (request.status === 403)
+                {
                     alert('username/password is incorrect');
                     
-                }else if (request.status === 500){
+                }else if (request.status === 500)
+                {
                     alert('something wrong with server, try again later!');
                 }
             }
             
         };
-    };
-    var username = document.getElementbyId('username').value;
+    var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
     console.log(username);
     console.log(password);
     request.open('POST', 'http//:namrata70.imad.hasura-app.io/login', true);
     request.setRequestHeader('content-type', 'application/JSON');
     request.send(JSON.stringify({username: username, password:password}));
+    };
     
