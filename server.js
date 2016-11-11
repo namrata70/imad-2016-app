356f89ec1b47573445ccda6be650245278f59fa3
namrata70 <dkritikar4sv@gmail.com>
2016-11-11 22:08:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -127,7 +127,6 @@ app.get('/articles/:articleName', function (req, res) {
         res.send(createTemplate(articleData));
     }
    });
-    res.send(createTemplate(articles[articleData]));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
