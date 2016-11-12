835e4ea87019843c04393f20476e9000bb5b4395
namrata70 <dkritikar4sv@gmail.com>
2016-11-12 16:48:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -199,7 +199,8 @@ app.get('/articles/:articleName', function (req, res) {
     }  else
     if(result.rows.length === 0){
         res.status(404).send('Article not found');
-    }else {
+    }else
+    {
         var articleData = result.rows[0];
         res.send(createTemplate(articleData));
     }
