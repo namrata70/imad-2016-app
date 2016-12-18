250107ace2f7ee55a2a07382be0f6f02397a9f7d
namrata70 <dkritikar4sv@gmail.com>
2016-12-18 11:46:08 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -116,7 +116,7 @@ function loadArticles () {
                 var articleData = JSON.parse(this.responseText);
                 for (var i=0; i< articleData.length; i++) {
                     content += `<li>
-                    <a style = text-decoration:none; href="/articles/${articleData[i].title}">${articleData[i].heading}</a>
+                    <a href="/articles/${articleData[i].title}">${articleData[i].heading}</a>
                     (${articleData[i].date.split('T')[0]})</li>`;
                 }
                 content += "</ul>";
